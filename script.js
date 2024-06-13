3d850a8754625f2bc3f580c2411c1b6a7352b09e
Vulth01 <damienharwood14@gmail.com>
2024-06-13 14:11:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,65 +9,93 @@ document.addEventListener("DOMContentLoaded", function() {
     const gameHeight = document.querySelector('.game-container').offsetHeight;
     const playerStartingHealth = 100;
     
-    var player1object = [ {height: 50, width: 50, health: 100} ];
-    var player2object = [ {height: 50, width: 50, health: 100} ];
+    var player1object = { height: 50, width: 50, health: 100, blocking: false };
+    var player2object = { height: 50, width: 50, health: 100, blocking: false };
+    let player2LastKnownLeft = null;
+    let player2LastKnownTop = null;
 
     let player1Movement = { left: false, right: false, up: false, down: false };
     let player2Movement = { left: false, right: false, up: false, down: false };
 
-    // PLAYER 1 MOVEMENT
-    function movePlayer1() {
+    const times = [];
+    
+    var fps;
 
-        if(player1object.health <= 0){
-            player2.classList.add('dead-player1');
+    function refreshLoop() {
+        window.requestAnimationFrame(() => {
+            const now = performance.now();
+            while (times.length > 0 && times[0] <= now - 1000) {
+                times.shift();
+            }
+            times.push(now);
+            fps = times.length;
+
+            document.getElementById('fpsCounter').textContent = 'FPS: ' + fps;
+
+            refreshLoop();
+        });
+    }
+
+    refreshLoop();
+
+    function movePlayer1() {
+        if(player1object.health <= 0 && !player1.classList.contains('dead-player1')){
+            player1.classList.add('dead-player1');
         }
 
-        if(player1.offsetLeft > gameWidth/2 - 50)                                            //Restrict to right side of screen
+        if (player1.offsetLeft > gameWidth/2 - 50 && player1object.health > 0)
         {
             player1.style.left = player1.offsetLeft - 5 + 'px';
         }
 
-        if (player1Movement.left && player1.offsetLeft > 0) {
+        if (player1Movement.left && player1.offsetLeft > 0 && !player1object.blocking && player1object.health > 0) {
             player1.style.left = player1.offsetLeft - 5 + 'px';
         }
-        if (player1Movement.right && player1.offsetLeft + playerWidth < gameWidth) {
+        if (player1Movement.right && player1.offsetLeft + playerWidth < gameWidth && !player1object.blocking && player1object.health > 0) {
             player1.style.left = player1.offsetLeft + 5 + 'px';
         }
-        if (player1Movement.up && player1.offsetTop > 0) {
+        if (player1Movement.up && player1.offsetTop > 0 && !player1object.blocking && player1object.health > 0) {
             player1.style.top = player1.offsetTop - 5 + 'px';
         }
-        if (player1Movement.down && player1.offsetTop + playerHeight < gameHeight) {
+        if (player1Movement.down && player1.offsetTop + playerHeight < gameHeight && !player1object.blocking && player1object.health > 0) {
             player1.style.top = player1.offsetTop + 5 + 'px';
         }
+
+        document.getElementById('player1Health').textContent = player1object.health;
     }
 
-    // PLAYER 2 MOVEMENT
     function movePlayer2() {
-        
-        if(player1object.health <= 0){
+        if(player2object.health <= 0 && !player2.classList.contains('dead-player2')){
+            player2LastKnownLeft = player2.offsetLeft;
+            player2LastKnownTop = player2.offsetTop;
             player2.classList.add('dead-player2');
+            player2.style.left = player2LastKnownLeft + 'px';
+            player2.style.top = player2LastKnownTop + 'px';
         }
         
-        if(player2.offsetLeft < gameWidth/2)                                            //Restrict to right side of screen
+        if(player2.offsetLeft < gameWidth/2 && player2object.health > 0)
         {
             player2.style.left = player2.offsetLeft + 5 + 'px';
         }
 
-        if (player2Movement.left && player2.offsetLeft > 0) {
+        if (player2Movement.left && player2.offsetLeft > 0 && !player2object.blocking && player2object.health > 0) {
             player2.style.left = player2.offsetLeft - 5 + 'px';
         }
-        if (player2Movement.right && player2.offsetLeft + playerWidth < gameWidth) {
+        if (player2Movement.right && player2.offsetLeft + playerWidth < gameWidth && !player2object.blocking && player2object.health > 0) {
             player2.style.left = player2.offsetLeft + 5 + 'px';
         }
-        if (player2Movement.up && player2.offsetTop > 0) {
+        if (player2Movement.up && player2.offsetTop > 0 && !player2object.blocking && player2object.health > 0) {
             player2.style.top = player2.offsetTop - 5 + 'px';
         }
-        if (player2Movement.down && player2.offsetTop + playerHeight < gameHeight) {
+        if (player2Movement.down && player2.offsetTop + playerHeight < gameHeight && !player2object.blocking && player2object.health > 0) {
             player2.style.top = player2.offsetTop + 5 + 'px';
         }
+
+        document.getElementById('player2Health').textContent = player2object.health;
     }
 
-    // Check collision between projectile and player
+
+
     function checkCollision(projectile, player) {
         const projectileRect = projectile.getBoundingClientRect();
         const playerRect = player.getBoundingClientRect();
@@ -80,158 +108,207 @@ document.addEventListener("DOMContentLoaded", function() {
         );
     }
 
-    // Update player health
     function updatePlayerHealth(player, health) {
-        
-        // Update player1 object
         if (player === player1) {
-            player1object[0].health = health;
-            
-        }
-        
-        // Update player2 object
-        else if (player === player2) {
-            player2object[0].health = health;
-        }
-
-        player.classList.add("check-player2");
-        setTimeout(() => { player.classList.remove("check-player2"); }, 400); 
-
-        // Check if player is dead
-        if (health <= 0) {
-            player.classList.add("check-player2");
-            player.classList.add('dead-player2');
+            player1object.health = health;
+            player1.classList.add("check-player1");
+            setTimeout(() => { player1.classList.remove("check-player1"); }, 400);
+            if (health <= 0) {
+                player1.classList.add("dead-player1");
+                player1object.health = 0; 
+            } else {
+                player1.classList.remove("dead-player1"); 
+            }
+        } else if (player === player2) {
+            player2object.health = health;
+            player2.classList.add("check-player2");
+            setTimeout(() => { player2.classList.remove("check-player2"); }, 400);
+            if (health <= 0) {
+                player2.classList.add("dead-player2");
+                player2object.health = 0;
+            } else {
+                player2.classList.remove("dead-player2");
+            }
         }
     }
 
-    // PLAYER 1 PROJECTILE
+
+
     function shootProjectilePlayer1() {
-        const projectile = document.createElement('div');
-        projectile.classList.add('projectile');
-        projectile.style.left = player1.offsetLeft + playerWidth / 2 + 'px';
-        projectile.style.top = player1.offsetTop + playerHeight / 2 + 'px';
-        projectilesContainer.appendChild(projectile);
+        if (!player1.classList.contains('block-player1')) {
+            const projectile = document.createElement('div');
+            projectile.classList.add('projectile');
+            projectile.style.left = player1.offsetLeft + playerWidth / 2 + 'px';
+            projectile.style.top = player1.offsetTop + playerHeight / 2 + 'px';
+            projectilesContainer.appendChild(projectile);
 
-        const projectileInterval = setInterval(() => {
-            projectile.style.left = parseInt(projectile.style.left || 0) + 5 + 'px';
-        
-            // Check collision with player 2
-            if (checkCollision(projectile, player2)) {
+            const projectileInterval = setInterval(() => {
+                projectile.style.left = parseInt(projectile.style.left || 0) + 5 + 'px';
+
+                if (player2object.health > 0 && !player2.classList.contains('block-player2') && checkCollision(projectile, player2)) {
+                    clearInterval(projectileInterval);
+                    projectilesContainer.removeChild(projectile);
+                    updatePlayerHealth(player2, player2object.health - 10); 
+                }
+
+                if (parseInt(projectile.style.top) < -10) {
+                    clearInterval(projectileInterval);
+                    projectilesContainer.removeChild(projectile);
+                }
+            }, 16);
+
+            setTimeout(() => {
                 clearInterval(projectileInterval);
                 projectilesContainer.removeChild(projectile);
-                updatePlayerHealth(player2, player2object[0].health - 10); // Decrease player 2 health
-            }
-        
-            if (parseInt(projectile.style.top) < -10) {
-                clearInterval(projectileInterval);
-                projectilesContainer.removeChild(projectile);
-            }
-        }, 16);
+            }, 5000);
+        }
     }
 
-    // PLAYER 2 PROJECTILE
     function shootProjectilePlayer2() {
-        const projectile = document.createElement('div');
-        projectile.style.backgroundColor = '#00ff00';
-        projectile.classList.add('projectile');
-        projectile.style.left = player2.offsetLeft + playerWidth / 2 + 'px';
-        projectile.style.top = player2.offsetTop + playerHeight / 2 + 'px';
-        projectilesContainer.appendChild(projectile);
+        if (!player2.classList.contains('block-player2')) {
+            const projectile = document.createElement('div');
+            projectile.style.backgroundColor = '#00ff00';
+            projectile.classList.add('projectile');
+            projectile.style.left = player2.offsetLeft + playerWidth / 2 + 'px';
+            projectile.style.top = player2.offsetTop + playerHeight / 2 + 'px';
+            projectilesContainer.appendChild(projectile);
 
-        const projectileInterval = setInterval(() => {
-            projectile.style.left = parseInt(projectile.style.left || 0) - 5 + 'px';
-        
-            // Check collision with player 1
-            if (checkCollision(projectile, player1)) {
+            const projectileInterval = setInterval(() => {
+                projectile.style.left = parseInt(projectile.style.left || 0) - 5 + 'px';
+
+                if (player1object.health > 0 && !player1.classList.contains('block-player1') && checkCollision(projectile, player1)) {
+                    clearInterval(projectileInterval);
+                    projectilesContainer.removeChild(projectile);
+                    updatePlayerHealth(player1, player1object.health - 10); 
+                }
+
+                if (parseInt(projectile.style.top) > gameHeight) {
+                    clearInterval(projectileInterval);
+                    projectilesContainer.removeChild(projectile);
+                }
+            }, 16);
+
+            setTimeout(() => {
                 clearInterval(projectileInterval);
                 projectilesContainer.removeChild(projectile);
-                updatePlayerHealth(player1, player1object[0].health - 10); // Decrease player 1 health
-            }
-        
-            if (parseInt(projectile.style.top) > gameHeight) {
-                clearInterval(projectileInterval);
-                projectilesContainer.removeChild(projectile);
-            }
-        }, 16);
+            }, 10000);
+        }
     }
 
-
-    // PLAYER MOVEMENT EVENT LISTENERS
     document.addEventListener('keydown', function(event) {
-        switch (event.key) {
-            case 'a':
-                player1Movement.left = true;
-                break;
-            case 'd':
-                player1Movement.right = true;
-                break;
-            case 'w':
-                player1Movement.up = true;
-                break;
-            case 's':
-                player1Movement.down = true;
-                break;
-            case 'ArrowLeft':
-                player2Movement.left = true;
-                break;
-            case 'ArrowRight':
-                player2Movement.right = true;
-                break;
-            case 'ArrowUp':
-                player2Movement.up = true;
-                break;
-            case 'ArrowDown':
-                player2Movement.down = true;
-                break;
-            case ' ':
-                shootProjectilePlayer1();
-                break;
-            case '/':
-                shootProjectilePlayer2();
-                break;
-            case 'x':                                      //NUMPAD0 - NOT WORKING
-                player2.classList.add('block-player2');
-                break;
-            default:
-                break;
+        if (player1object.health > 0) {
+            switch (event.key) {
+                case 'a':
+                    if (!player1.classList.contains('block-player1')) {
+                        player1Movement.left = true;
+                    }
+                    break;
+                case 'd':
+                    if (!player1.classList.contains('block-player1')) {
+                        player1Movement.right = true;
+                    }
+                    break;
+                case 'w':
+                    if (!player1.classList.contains('block-player1')) {
+                        player1Movement.up = true;
+                    }
+                    break;
+                case 's':
+                    if (!player1.classList.contains('block-player1')) {
+                        player1Movement.down = true;
+                    }
+                    break;
+                case ' ':
+                    if (!player1.classList.contains('block-player1')) {
+                        shootProjectilePlayer1();
+                    }
+                    break;
+                case 'f':
+                    player1.classList.toggle('block-player1');
+                    player1object.blocking = !player1object.blocking;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (player2object.health > 0) {
+            switch (event.key) {
+                case 'ArrowLeft':
+                    if (!player2.classList.contains('block-player2')) {
+                        player2Movement.left = true;
+                    }
+                    break;
+                case 'ArrowRight':
+                    if (!player2.classList.contains('block-player2')) {
+                        player2Movement.right = true;
+                    }
+                    break;
+                case 'ArrowUp':
+                    if (!player2.classList.contains('block-player2')) {
+                        player2Movement.up = true;
+                    }
+                    break;
+                case 'ArrowDown':
+                    if (!player2.classList.contains('block-player2')) {
+                        player2Movement.down = true;
+                    }
+                    break;
+                case 'o':
+                    if (!player2.classList.contains('block-player2')) {
+                        shootProjectilePlayer2();
+                    }
+                    break;
+                case 'p':
+                    player2.classList.toggle('block-player2');
+                    player2object.blocking = !player2object.blocking;
+                    break;
+                default:
+                    break;
+            }
         }
     });
 
     document.addEventListener('keyup', function(event) {
-        switch (event.key) {
-            case 'a':
-                player1Movement.left = false;
-                break;
-            case 'd':
-                player1Movement.right = false;
-                break;
-            case 'w':
-                player1Movement.up = false;
-                break;
-            case 's':
-                player1Movement.down = false;
-                break;
-            case 'ArrowLeft':
-                player2Movement.left = false;
-                break;
-            case 'ArrowRight':
-                player2Movement.right = false;
-                break;
-            case 'ArrowUp':
-                player2Movement.up = false;
-                break;
-            case 'ArrowDown':
-                player2Movement.down = false;
-                break;
-            case 'x':
-                player2.classList.remove('block-player2');
-                break;
-            default:
-                break;
+        if (player1object.health > 0) {
+            switch (event.key) {
+                case 'a':
+                    player1Movement.left = false;
+                    break;
+                case 'd':
+                    player1Movement.right = false;
+                    break;
+                case 'w':
+                    player1Movement.up = false;
+                    break;
+                case 's':
+                    player1Movement.down = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (player2object.health > 0) {
+            switch (event.key) {
+                case 'ArrowLeft':
+                    player2Movement.left = false;
+                    break;
+                case 'ArrowRight':
+                    player2Movement.right = false;
+                    break;
+                case 'ArrowUp':
+                    player2Movement.up = false;
+                    break;
+                case 'ArrowDown':
+                    player2Movement.down = false;
+                    break;
+                default:
+                    break;
+            }
         }
     });
 
-    // Game loop
     setInterval(movePlayer1, 16);
     setInterval(movePlayer2, 16);
 });
